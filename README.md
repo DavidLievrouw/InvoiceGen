2a5cfb11768520bcf914347790fe5e6328689cd0
davidlievrouw <david.lievrouw@gmail.com>
2015-08-31 21:31:09 +0200

    Add initial version of integration tests that test if the data access works
    Former-commit-id: f9f747e6e7fe6eb14774176f33262a764bb7822e

--- README.md
+++ README.md
@@ -15,11 +15,13 @@
 
 ### Prerequisites
 * Browser with PDF support (or Acrobat Reader for IE)
-* 2007 Office System Driver: Data Connectivity Components: http://www.microsoft.com/en-us/download/details.aspx?id=23734
+* Microsoft Access 2013 Runtime: http://www.microsoft.com/en-us/download/details.aspx?id=39358
 * DropBox client software (for backup functionality)
 
 ### Developer notes
 * TBA
 
 ### Installation instructions
+* Install prerequisites:
+  * Microsoft Access 2013 Runtime
 * TBA
---
+++ src/InvoiceGen.IntegrationTests/DatabaseConfigurationReader.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Configuration;
+using System.IO;
+
+namespace DavidLievrouw.InvoiceGen {
+  public static class DatabaseConfigurationReader {
+    public static DatabaseConfiguration Read(System.Configuration.Configuration config) {
+      if (config == null) throw new ArgumentNullException("config");
+
+      var connectionStringSettings = config.ConnectionStrings.ConnectionStrings["AccessDB"];
+      if (connectionStringSettings == null) throw new SettingsPropertyNotFoundException("The connection string was not found in the settings.");
+      return new DatabaseConfiguration(connectionStringSettings);
+    }
+
+    public static DatabaseConfiguration Read() {
+      var configMap = new ExeConfigurationFileMap {
+        ExeConfigFilename = Path.GetFileName(typeof(DatabaseConfiguration).Assembly.Location) + ".config"
+      };
+      var config = ConfigurationManager.OpenMappedExeConfiguration(configMap, ConfigurationUserLevel.None);
+      return Read(config);
+    }
+  }
+}
