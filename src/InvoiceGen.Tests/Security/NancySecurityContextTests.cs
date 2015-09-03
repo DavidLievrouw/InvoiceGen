44b6fec7ab63fcd39c6bdd26eb63f36d664b1624
David Lievrouw <dlievrouw2@csc.com>
2015-09-03 16:50:06 +0200

    Abstract NancyContext into custom interfaces. Still need to replace the usages of NancyContext in code

---
+++ src/InvoiceGen.Tests/Security/NancySecurityContextTests.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using DavidLievrouw.InvoiceGen.Api.Handlers;
+using DavidLievrouw.InvoiceGen.Domain;
+using FakeItEasy;
+using Nancy;
+using NUnit.Framework;
+
+namespace DavidLievrouw.InvoiceGen.Security {
+  [TestFixture]
+  public class NancySecurityContextTests {
+    ISessionFromContextResolver _sessionFromContextResolver;
+    IInvoiceGenIdentityFactory _invoiceGenIdentityFactory;
+    NancyContext _nancyContext;
+    NancySecurityContext _sut;
+
+    [SetUp]
+    public void SetUp() {
+      _sessionFromContextResolver = _sessionFromContextResolver.Fake();
+      _invoiceGenIdentityFactory = _invoiceGenIdentityFactory.Fake();
+      _nancyContext = new NancyContext();
+      _sut = new NancySecurityContext(_nancyContext, _sessionFromContextResolver, _invoiceGenIdentityFactory);
+    }
+
+    [Test]
+    public void ConstructorTests() {
+      Assert.Throws<ArgumentNullException>(() => new NancySecurityContext(null, _sessionFromContextResolver, _invoiceGenIdentityFactory));
+      Assert.Throws<ArgumentNullException>(() => new NancySecurityContext(_nancyContext, null, _invoiceGenIdentityFactory));
+      Assert.Throws<ArgumentNullException>(() => new NancySecurityContext(_nancyContext, _sessionFromContextResolver, null));
+    }
+
+    [Test]
+    public void SetsUserInNancyContext() {
+      var user = new User();
+      var identity = new InvoiceGenIdentity(user);
+      ConfigureInvoiceGenIdentityFactory_ToReturn(user, identity);
+
+      _sut.AuthenticatedUser = user;
+      var actual = _nancyContext.CurrentUser;
+
+      Assert.That(actual, Is.Not.Null);
+      Assert.That(actual, Is.EqualTo(identity));
+    }
+
+    [Test]
+    public void GivenNullUser_SetsNullUserInNancyContext() {
+      ConfigureInvoiceGenIdentityFactory_ToReturn(null, null);
+
+      _sut.AuthenticatedUser = null;
+      var actual = _nancyContext.CurrentUser;
+
+      Assert.That(actual, Is.Null);
+    }
+
+    [Test]
+    public void WhenNoIdentityIsSetInNancyContext_ReturnsNullUser() {
+      _nancyContext.CurrentUser = null;
+      var actual = _sut.AuthenticatedUser;
+      Assert.That(actual, Is.Null);
+    }
+
+    [Test]
+    public void WhenInvalidIdentityIsSetInNancyContext_ReturnsNullUser() {
+      _nancyContext.CurrentUser = new FakeUserIdentity("Pol");
+      var actual = _sut.AuthenticatedUser;
+      Assert.That(actual, Is.Null);
+    }
+
+    [Test]
+    public void WhenIdentityIsSetInNancyContext_ReturnsUserFromIdentity() {
+      var user = new User();
+      var identity = new InvoiceGenIdentity(user);
+      _nancyContext.CurrentUser = identity;
+
+      var actual = _sut.AuthenticatedUser;
+      Assert.That(actual, Is.Not.Null);
+      Assert.That(actual, Is.EqualTo(user));
+    }
+
+    [Test]
+    public void ReturnsSessionFromResolver() {
+      var session = A.Fake<ISession>();
+      ConfigureSessionFromContextResolver_ToReturn(session);
+
+      var actual = _sut.Session;
+
+      Assert.That(actual, Is.Not.Null);
+      Assert.That(actual, Is.EqualTo(session));
+      A.CallTo(() => _sessionFromContextResolver.ResolveSession(_nancyContext))
+       .MustHaveHappened();
+    }
+
+    void ConfigureSessionFromContextResolver_ToReturn(ISession session) {
+      A.CallTo(() => _sessionFromContextResolver.ResolveSession(A<NancyContext>._))
+       .Returns(session);
+    }
+
+    void ConfigureInvoiceGenIdentityFactory_ToReturn(User user, InvoiceGenIdentity identity) {
+      A.CallTo(() => _invoiceGenIdentityFactory.Create(user))
+       .Returns(identity);
+    }
+  }
+}
---
+++ src/InvoiceGen/Security/NancySecurityContext.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using DavidLievrouw.InvoiceGen.Domain;
+using Nancy;
+
+namespace DavidLievrouw.InvoiceGen.Security {
+  public class NancySecurityContext : ISecurityContext {
+    readonly NancyContext _nancyContext;
+    readonly ISessionFromContextResolver _sessionFromContextResolver;
+    readonly IInvoiceGenIdentityFactory _invoiceGenIdentityFactory;
+
+    public NancySecurityContext(NancyContext nancyContext, ISessionFromContextResolver sessionFromContextResolver, IInvoiceGenIdentityFactory invoiceGenIdentityFactory) {
+      if (nancyContext == null) throw new ArgumentNullException("nancyContext");
+      if (sessionFromContextResolver == null) throw new ArgumentNullException("sessionFromContextResolver");
+      if (invoiceGenIdentityFactory == null) throw new ArgumentNullException("invoiceGenIdentityFactory");
+      _nancyContext = nancyContext;
+      _sessionFromContextResolver = sessionFromContextResolver;
+      _invoiceGenIdentityFactory = invoiceGenIdentityFactory;
+    }
+
+    public User AuthenticatedUser {
+      get {
+        var identity = _nancyContext.CurrentUser as InvoiceGenIdentity;
+        return identity == null
+          ? null
+          : identity.User;
+      }
+      set { _nancyContext.CurrentUser = _invoiceGenIdentityFactory.Create(value); }
+    }
+
+    public ISession Session {
+      get { return _sessionFromContextResolver.ResolveSession(_nancyContext); }
+    }
+  }
+}
